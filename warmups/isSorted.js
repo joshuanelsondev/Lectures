e51f8c3a97c3ebb152c37cc00c87e611dcb73f25
joshuanelsondev <joshuanelsondev@gmail.com>
2024-01-25 18:47:01 -0500

    Add two array warmups

---
+++ warmups/isSorted.js
@@ -0,0 +1,13 @@
+/** 
+Implement a function that checks if a given array of integers is sorted in non-decreasing order. Use the two pointers approach to solve this problem efficiently.
+ * @param {number[]} nums
+ * @returns {boolean}
+
+*/
+function isSorted(nums) {
+
+}
+
+// Example
+console.log(isSorted([1, 2, 3, 4, 5]));  // Should return true
+console.log(isSorted([5, 3, 7, 8, 10]));  // Should return false
---
+++ warmups/twoSumPair.js
@@ -0,0 +1,13 @@
+/**  
+Given a sorted array of distinct integers and a target sum, find a pair of elements in the array that add up to the target sum. Implement the solution using the two pointers technique.
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number[]}
+ */
+function twoSumPair(nums, target) {
+
+}
+
+// Example
+console.log(twoSumPair([1, 2, 3, 4, 5], 9));  // Should return [4, 5]
+console.log(twoSumPair([-2, 0, 2, 5, 7], 5));  // Should return [-2, 7]
